5de79e626174e45f72f83470fe18cd3c806a0f9d
vp.romesh.perera <romeshperera@virtusa.com>
2018-06-16 15:15:44 -0400

    First Commit

---
+++ NetService1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NetService1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ NetService1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetService1.WebApiApplication" Language="C#" %>
